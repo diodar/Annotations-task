222e07d411d687206ddd60e14bc56b2195af8b12
diodar <mikhailgalis@gmail.com>
2023-08-17 13:52:49 +0300

    Initial commit

---
+++ src/app/Product.java
@@ -0,0 +1,15 @@
+package app;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface Product {
+    String name() default "unknown";
+
+    int quota() default 0;
+}
